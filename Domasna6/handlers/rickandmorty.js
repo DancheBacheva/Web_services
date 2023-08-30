9c8e41e6ad91a06d1383d6e9d88c2487504e973f
DancheBacheva <danchebacheva@gmail.com>
2023-08-30 18:59:07 +0200

    mala promena

--- Domasna6/handlers/rickandmorty.js
+++ Domasna6/handlers/rickandmorty.js
@@ -6,11 +6,11 @@ const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fet
 //inicijalizirame prazen objekt narecen "cache" koj kje cuva keshirani podatoci 
 let cache = {}
 
-//"getCharacter" e asinhrona funkcija koja se povikuva na rutata "/api/v1/rickandmorty/character/:id"
+//"getData" e asinhrona funkcija koja se povikuva na rutata "/api/v1/rickandmorty/character/:id"
 //ne` povrzuva so url-to za prezemanje podatoci od API-tо RickN'Morty
 exports.getData = async (req, res) => {
-  let url = `https://rickandmortyapi.com/api/character`;
-//prvo proveruva dali ima keshirani podatoci za daden ID. Ako keshiranite podatoci postojat i ne se zastareni, localCache se setira da bide null, odnosno da gi izbrishe podatocite zacuvani od prethodno
+  let url = `https://rickandmortyapi.com/api/character/${req.params.id}`;
+//prvo proveruva dali ima keshirani podatoci za dadeno ID. Ako keshiranite podatoci postojat i ne se zastareni, localCache se setira da bide null, odnosno da gi izbrishe podatocite zacuvani od prethodno
   if( cache[req.params.id] &&
       cache[req.params.id].cacheTime !== null &&
       cache[req.params.id].cacheTime + 60 * 1000 < new Date().getTime()
